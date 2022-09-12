03dbd3c88e872e20a955f9246be691f88658eb99
Mike Davis <micdavis93@gmail.com>
2022-09-12 01:13:26 -0600

    commit tree f054a2f0a975e284335cfb38596df457e1c7ce9c

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jeff",
+    streetAddress: "123 Elm St"
+};
+
+function updateEmployeeWithKeyAndValue(employee, streetAddress) {
+    return {
+        ...employee,
+        [streetAddress]: "11 Broadway",
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress) {
+    employee[streetAddress] = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, streetAddress, name) {
+    const newEmployee = {...employee};
+    delete newEmployee.streetAddress;
+    return {
+        [name]: undefined,
+    }
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, streetAddress, name) {
+    employee[name] = undefined;
+    delete employee[streetAddress];
+    const newEmployee = {...employee};
+    return employee;
+}
